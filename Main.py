22f0aba961095f95bdb3c9274627fae7753573bb
Edmund Dable-Heath <eddableheath@gmail.com>
2019-03-01 14:27:59 +0000

    Added main file

---
+++ Main.py
@@ -0,0 +1,16 @@
+'''Modelling the Gibbs Sampler to Measure the Autocorrelation'''
+
+import numpy as np
+
+## Pick inputs:
+
+# Basis (standard is 2-d integer lattice):
+B = np.array([[1,0],
+              [0,1]])
+
+# Mean:
+Mu = np.array([0,0])
+
+# Standard Deviation:
+Sigma = 1
+
