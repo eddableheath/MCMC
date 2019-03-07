b9cafb660ffe39d3031b45de85f87832f52f7515
Edmund Dable-Heath <eddableheath@gmail.com>
2019-03-07 17:57:07 +0000

    probability generation function working and testing for multiple dimensions

--- Functions.py
+++ Functions.py
@@ -48,26 +48,51 @@ def norm_con(basis, vector, mean, index, sd, cutoff_param):
     total = 0
     ''' Ascending from the mean '''
     k = round(mean[index])
-    while True:
+    while tail_cutoff(basis,vector,k,index,mean,sd,cutoff_param,1):
         total += math.exp(-(1/(2*(sd**2)))*simple_sum(basis,vector,mean,index,k))
         k += 1
-        if not tail_cutoff(basis,vector,k,index,mean,sd,cutoff_param,1):
-            break
     ''' Descending from Mean '''
     l = round(mean[index]) - 1
-    while True:
+    while tail_cutoff(basis,vector,l,index,mean,sd,cutoff_param,0):
         total += math.exp(-(1/(2*(sd**2)))*simple_sum(basis,vector,mean,index,l))
         l -= 1
-        if not tail_cutoff(basis,vector,l,index,mean,sd,cutoff_param,0):
-            break
     return total
 
 
 
-b = np.array([[1, 0],
-              [0, 1]])
-v = np.array([5, -7])
-m = np.array([0, 0])
-i = pick_index(2)
 
-print(norm_con(b,v,m,i,1,13))
+# Generating the probabilities for each pick:
+def prob_gen(basis,vector,mean,index,sd,cutoff_param):
+    # compute the normalisation constant from the distribution
+    N = norm_con(basis,vector,mean,index,sd,cutoff_param)
+    # initialising numpy array (0 vector as slightly bodge)
+    dist = [[0,0]]
+    ''' Ascending from the mean '''
+    k = round(mean[index])
+    while tail_cutoff(basis,vector,k,index,mean,sd,cutoff_param,1):
+        Pk = (math.exp(-(1/(2*(sd**2)))*simple_sum(basis,vector,mean,index,k)))/N
+        dist.append([k,Pk])
+        k += 1
+    ''' Descending from the mean '''
+    l = round(mean[index])-1
+    while tail_cutoff(basis,vector,l,index,mean,sd,cutoff_param,0):
+        Pl = (math.exp(-(1/(2*(sd**2)))*simple_sum(basis,vector,mean,index,l)))/N
+        dist.append([l,Pl])
+        l -= 1
+    del dist[0]
+    distro = np.array(dist)
+    return distro
+
+b = np.array([[1,0,0],
+              [0,1,0],
+              [0,0,1]])
+v = np.array([4,-7,0])
+m = np.array([0.5,0.5,0])
+i = pick_index(3)
+var = 0.4
+cutoff = 15
+
+#print(norm_con(b,v,m,i,var,cutoff))
+
+print(prob_gen(b,v,m,i,var,cutoff))
+print(np.sum(prob_gen(b,v,m,i,var,cutoff),axis=0))
