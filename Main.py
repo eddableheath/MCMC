c2f9b2c33ced4957ad2307b8f996c8308e7b746f
Edmund Dable-Heath <eddableheath@gmail.com>
2019-03-08 16:20:59 +0000

    Attempted to get main gibbs function working.

--- Main.py
+++ Main.py
@@ -5,13 +5,20 @@ import Functions as Fn
 
 # Gibbs updater
 def gibbs_update(basis,dimension,vector,mean,variance,cutoff):
+    vec = vector
     for i in range(dimension+1):
         n = Fn.pick_index(dimension)
-        d = Fn.prob_gen(basis,vector,mean,n,variance,cutoff)
+        d = Fn.prob_gen(basis,vec,mean,n,variance,cutoff)
         ''' Picking from prob gen '''
-        np.put(vector,n,np.random.choice(d[:,0],1,p=d[:,1]))
-    return vector
+        np.put(vec,n,np.random.choice(d[:,0],1,p=d[:,1]))
+    return vec
 
+def gibbs(basis,dimension,vector,mean,variance,cutoff,total_run):
+    data = [[0,vector]]
+    #for i in range(1,total_run):
+     #   new_vec = gibbs_update(basis,dimension,data[i-1][1],mean,variance,cutoff)
+      #  data.append([i,new_vec])
+    return data[0][1]
 
 
 # Test data
@@ -24,6 +31,14 @@ m = np.array([0.5, 0.5, 0, 3])
 var = 0.4
 cutoff = 15
 
-print(gibbs_update(b,4,v,m,var,cutoff))
+
+print(gibbs(b,4,v,m,var,cutoff,5))
+print(v)
+gibbs_update(b,4,v,m,var,cutoff)
+print(v)
+gibbs_update(b,4,v,m,var,cutoff)
+print(v)
+print(gibbs(b,4,v,m,var,cutoff,5))
+print(v)
 
 
