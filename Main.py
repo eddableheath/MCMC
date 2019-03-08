1e6c02d370cf077102c52ed943b23780cfb8942e
Edmund Dable-Heath <eddableheath@gmail.com>
2019-03-08 13:38:15 +0000

    Added Gibbs updater function

--- Main.py
+++ Main.py
@@ -1,16 +1,29 @@
 '''Modelling the Gibbs Sampler to Measure the Autocorrelation'''
 
 import numpy as np
+import Functions as Fn
 
-## Pick inputs:
+# Gibbs updater
+def gibbs_update(basis,dimension,vector,mean,variance,cutoff):
+    for i in range(dimension+1):
+        n = Fn.pick_index(dimension)
+        d = Fn.prob_gen(basis,vector,mean,n,variance,cutoff)
+        ''' Picking from prob gen '''
+        np.put(vector,n,np.random.choice(d[:,0],1,p=d[:,1]))
+    return vector
 
-# Basis (standard is 2-d integer lattice):
-B = np.array([[1,0],
-              [0,1]])
 
-# Mean:
-Mu = np.array([0,0])
 
-# Standard Deviation:
-Sigma = 1
+# Test data
+b = np.array([[1, 0, 0, 0],
+              [0, 1, 0, 0],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1]])
+v = np.array([4, -7, 8, -2])
+m = np.array([0.5, 0.5, 0, 3])
+var = 0.4
+cutoff = 15
+
+print(gibbs_update(b,4,v,m,var,cutoff))
+
 
